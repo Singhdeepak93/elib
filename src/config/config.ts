668b9299dba808fc52adef44ea06306b52840d0e
Singhdeepak93 <deepak01234567890rajput@gmail.com>
2024-04-12 00:23:42 +0530

    add register creation

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ const _config = {
     port : process.env.PORT,
     databaseUrl:process.env.DB_Connection,
     node_env: process.env.NODE_ENV,
+    jwtSecret:process.env.JWT_SECRET,
 };
 
 
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModal from "./userModal";
+import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
+import { User } from "./userTypes";
 
 
 const createUser = async (req: Request,res: Response,next:NextFunction) =>{
@@ -8,12 +13,43 @@ const createUser = async (req: Request,res: Response,next:NextFunction) =>{
     //validation 
     //u can use express validater
     if(!name || !email || !password){
-        const error = createHttpError(404, "All field are requied");
+        const error = createHttpError(400, "All field are requied");
         return next(error);
+    }
+    //Database call
+    try {
+        const user = await userModal.findOne({email});
+    if(user){
+        const error = createHttpError(400,"user already exits");
+        return next(error);
+    }
+    } catch (error) {
+        return next(createHttpError(500,"Error will getting user"));
     }
 
 
-    res.json({message:"User Created sucessfully"});
+    
+    //bcrypt hash
+    const hashedPassword = await bcrypt.hash(password, 10);
+    let newUser : User;
+    try {
+       newUser = await userModal.create({
+        name, email, password: hashedPassword
+    }); 
+    } catch (error) {
+        return next(createHttpError(500,"Error will getting user"));
+    }
+   
+
+    //token generation
+    try {
+        const token = sign({sub:newUser._id}, config.jwtSecret as string, {expiresIn: '7d'});
+          res.json({accessToken: token});
+        
+    } catch (error) {
+        return next(createHttpError(500,"Error while singin jwtToken")); 
+    }
+  
 }
 
 export  {createUser};
