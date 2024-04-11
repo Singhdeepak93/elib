d1ff421dab429fbbdc3e87cde02f94ae7407666c
Singhdeepak93 <deepak01234567890rajput@gmail.com>
2024-04-12 00:49:13 +0530

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -44,12 +44,39 @@ const createUser = async (req: Request,res: Response,next:NextFunction) =>{
     //token generation
     try {
         const token = sign({sub:newUser._id}, config.jwtSecret as string, {expiresIn: '7d'});
-          res.json({accessToken: token});
+          res.status(201).json({accessToken: token});
         
     } catch (error) {
         return next(createHttpError(500,"Error while singin jwtToken")); 
     }
   
 }
+const loginUser = async (req: Request,res: Response,next:NextFunction) =>{
 
-export  {createUser};
+    const {email,password} = req.body;
+    //validation 
+    //u can use express validater
+    if(!email || !password){
+        const error = createHttpError(400, "All field are requied");
+        return next(error);
+    }
+    //Database call
+     
+    
+        const user = await userModal.findOne({email});
+    if(!user){
+        const error = createHttpError(404,"user not Found");
+        return next(error);
+    }
+      //bcrypt hash
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        return next(createHttpError(500,"user or password invalid")); 
+    }
+   //token generation
+        const token = sign({sub:user._id}, config.jwtSecret as string, {expiresIn: '7d'});
+          res.json({accessToken: token});
+     
+}
+
+export  {createUser, loginUser};
