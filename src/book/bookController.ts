77c435f37c71b9d2e0181486cdb36f3f392ef1d5
Singhdeepak93 <deepak01234567890rajput@gmail.com>
2024-04-14 19:36:43 +0530

    add jwt middleware verification

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -2,39 +2,65 @@ import { NextFunction, Request, Response } from "express";
 import cloudinary from "../config/cloudinary";
 import path from "node:path";
 import createHttpError from "http-errors";
+import fs from 'node:fs';
+import bookModel from "./bookModel";
 
 const createBook =async (req: Request,res: Response,next:NextFunction) => {
+  const {title,genre} = req.body;
+  const files = req.files as {[fieldname: string]: Express.Multer.File[] };
+
+  const coverImageMimeType = files.coverImage[0].mimetype.split('/').at(-1);
+  const fileName = files.coverImage[0].filename;
+  const filePath = path.resolve(__dirname,'../../public/data/uploads',fileName );
   try {
-    
-    const files = req.files as {[fieldname: string]: Express.Multer.File[] };
-
-    const coverImageMimeType = files.coverImage[0].mimetype.split('/').at(-1);
-    const fileName = files.coverImage[0].filename;
-    const filePath = path.resolve(__dirname,'../../public/data/uploads',fileName ) 
-
-
     const uploadResult = await cloudinary.uploader.upload(filePath, {
-       filename_override: fileName,
-       folder:'book-covers',
-     formate: coverImageMimeType,
-    });
-    
-    const bookFileName = files.file[0].filename;
-    const bookFilePath = path.resolve(__dirname,'../../public/data/uploads',bookFileName ) 
-  const bookFileUploadResult= await cloudinary.uploader.upload(bookFilePath,{
-    resource_type:"raw",
-    filename_override:bookFileName,
-    folder:"book-pdfs",
-    format:"pdf",
-  });
- 
+      filename_override: fileName,
+      folder:'book-covers',
+    formate: coverImageMimeType,
+   });
+   
+   const bookFileName = files.file[0].filename;
+   const bookFilePath = path.resolve(__dirname,'../../public/data/uploads',bookFileName ) 
+   const bookFileUploadResult= await cloudinary.uploader.upload(bookFilePath,{
+   resource_type:"raw",
+   filename_override:bookFileName,
+   folder:"book-pdfs",
+   format:"pdf",
+ });
+ console.log("uploadResult",uploadResult);
+ console.log("bookFileUploadResult",bookFileUploadResult);
 
-    res.json({});
+
+ 
     
+  //create upload file and url in mongo db  store
+ //@ts-ignore
+  // console.log('userId', req.userId);
+
+  const newBook = await bookModel.create({
+    title,
+    genre,
+    author:"66182edebb3511e5786236ae",
+    coverImage:uploadResult.secure_url,
+    file:bookFileUploadResult.secure_url,
+
+  });
+  console.log("newBook", newBook);
+
+  //delete the temp file 
+  try {
+    await fs.promises.unlink(filePath);
+    await fs.promises.unlink(bookFilePath);
   } catch (error) {
-    return next(createHttpError(500,'Error while uploading the file'));
-    
+    return next(createHttpError(500,'Error while temp file Delete'))
   }
+  
+
+   res.status(201).json({id:newBook._id});
+  } catch (error) {
+    return next(createHttpError(500,'Error while uploading the file'))
+  }
+ 
 
    
 };
